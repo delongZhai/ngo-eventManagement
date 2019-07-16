b4f42391efbf64e8cded62e2ab1bcacdc112ab47
delongZhai <zhai226106433@gmail.com>
2019-07-16 16:00:44 -0400

    merging- simplified way

---
+++ node-server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+//const routes = require("./routes/routes.js");
+
+const app = express();
+//app.use("/",routes);
+
+const db = "mongodb://localhost:27017/ngo-event"
+const port = process.env.PORT||7000;
+
+mongoose.connect(db,err=>
+{
+    if(err)
+    {
+        console.log("Error! "+err);
+    }
+    else
+    {
+        console.log("Connected to mongoDB")
+    }
+})
+
+app.get("/",(req,res)=>
+{
+    res.send("Default Route");
+})
+
+app.listen(port,()=>console.log(`Listening on port ${port}...`));
