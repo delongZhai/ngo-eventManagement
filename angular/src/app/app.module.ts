ea46eb26fa3029fc676b5bc1c95907d1aca039ce
Delong Zhai <Zhai226106433@gmail.com>
2019-07-17 23:00:28 -0400

    completed admin component -- made few changes to admin-user compo.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminUserComponent } from './admin-user/admin-user.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminUserComponent
+    AdminUserComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
