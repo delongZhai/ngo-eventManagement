91170212ed55be99f0f91873bf5809a320fd1b15
greygxz <greygxz@gmail.com>
2019-07-23 13:47:37 -0400

    complete?

---
+++ angular/src/app/event-comp/user-view/eventregisterconfirmation/eventregisterconfirmation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventregisterconfirmation',
+  templateUrl: './eventregisterconfirmation.component.html',
+  styleUrls: ['./eventregisterconfirmation.component.css']
+})
+export class EventregisterconfirmationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular/src/app/event-comp/user-view/eventregistrationdetail2/eventregistrationdetail2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventregistrationdetail2',
+  templateUrl: './eventregistrationdetail2.component.html',
+  styleUrls: ['./eventregistrationdetail2.component.css']
+})
+export class Eventregistrationdetail2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
